eaca8f45810b8375bc38849bef9fb5f2ca282d2b
EldiRangel <eldi456.rangel@gmail.com>
2026-02-26 12:04:57 -0800

    Delete Documents/NetBeansProjects/Supermercado_CRUD directory

--- Documents/NetBeansProjects/Supermercado_CRUD/src/main/java/com/mycompany/supermercado_crud/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.supermercado_crud {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.mycompany.supermercado_crud to javafx.fxml;
-    exports com.mycompany.supermercado_crud;
-}
