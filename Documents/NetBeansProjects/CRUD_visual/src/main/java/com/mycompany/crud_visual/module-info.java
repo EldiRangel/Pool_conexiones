81f989ddd345bcebffd4be186fbd99c4bd733694
EldiRangel <eldi456.rangel@gmail.com>
2026-02-26 12:05:29 -0800

    .

--- Documents/NetBeansProjects/CRUD_visual/src/main/java/com/mycompany/crud_visual/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.mycompany.crud_visual {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    requires java.desktop;
-
-    opens com.mycompany.crud_visual to javafx.fxml;
-    exports com.mycompany.crud_visual;
-}
