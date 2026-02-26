7a248321143a033380c9fea34d93f8d487f3aeb7
EldiRangel <eldi456.rangel@gmail.com>
2026-02-26 12:04:37 -0800

    .

--- Documents/NetBeansProjects/UniversalMarketCRUD/src/main/java/com/mycompany/universalmarketcrud/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.universalmarketcrud {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.mycompany.universalmarketcrud to javafx.fxml;
-    exports com.mycompany.universalmarketcrud;
-}
